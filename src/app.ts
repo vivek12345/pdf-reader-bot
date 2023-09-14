cde8085deb81b842e236cc0eac38cf74339c54f3
Vivek Nayyar <vivek.of.nayyar@gmail.com>
2023-09-15 04:26:22 +0700

    feat: init repo with a chatbot to answer all budget related questions

--- src/app.ts
+++ src/app.ts
@@ -1,16 +1,69 @@
+import { PDFLoader } from "langchain/document_loaders/fs/pdf";
+import { RecursiveCharacterTextSplitter } from "langchain/text_splitter";
+import { OpenAIEmbeddings } from "langchain/embeddings/openai";
+import { HNSWLib } from "langchain/vectorstores/hnswlib";
+import { RetrievalQAChain } from "langchain/chains";
 import { OpenAI } from "langchain/llms/openai";
 import * as dotenv from "dotenv";
-dotenv.config();
-if (!process.env.OPENAI_API_KEY) {
-  // if you are missing the OPENAI_API_KEY environment variable you probably need to create your .env file in the root of the project
-  throw new Error("Missing OPENAI_API_KEY environment variable");
-}
-export const run = async () => {
-  const model = new OpenAI({
-    openAIApiKey: process.env.OPENAI_API_KEY,
-  });
-  const res = await model.call("What is the meaning of life ?");
-  console.log({ res });
-};
 
-run();
+dotenv.config();
+
+const loader = new PDFLoader("src/documents/budget_speech.pdf");
+
+const docs = await loader.load();
+
+// splitter function
+const splitter = new RecursiveCharacterTextSplitter({
+  chunkSize: 1000,
+  chunkOverlap: 20,
+});
+
+// created chunks from pdf
+const splittedDocs = await splitter.splitDocuments(docs);
+
+const embeddings = new OpenAIEmbeddings();
+
+const vectorStore = await HNSWLib.fromDocuments(
+  splittedDocs,
+  embeddings
+);
+
+const vectorStoreRetriever = vectorStore.asRetriever();
+const model = new OpenAI({
+  modelName: 'gpt-3.5-turbo'
+});
+
+const chain = RetrievalQAChain.fromLLM(model, vectorStoreRetriever);
+
+const question = 'What is the theme of G20?';
+
+const answer = await chain.call({
+  query: question
+});
+
+console.log({
+  question,
+  answer
+});
+
+const question1 = 'What is the budget allocated for railways?';
+const answer1 = await chain.call({
+  query: question1
+});
+
+console.log({
+  question: question1,
+  answer: answer1
+});
+
+const question2 = 'what is PM Matsya Sampada Yojana?'
+const answer2 = await chain.call({
+  query: question2
+});
+console.log({
+  question: question2,
+  answer: answer2
+});
+
+
+
