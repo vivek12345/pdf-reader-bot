045b19a07a48b7a9fe537cf218c7e19096db73e1
Vivek Nayyar <vivek.of.nayyar@gmail.com>
2023-10-01 21:39:57 +0700

    feat: added csv chatbot support

--- src/app.ts
+++ src/app.ts
@@ -1,69 +1,66 @@
-import { PDFLoader } from "langchain/document_loaders/fs/pdf";
+import { CSVLoader } from "langchain/document_loaders/fs/csv";
 import { RecursiveCharacterTextSplitter } from "langchain/text_splitter";
 import { OpenAIEmbeddings } from "langchain/embeddings/openai";
 import { HNSWLib } from "langchain/vectorstores/hnswlib";
 import { RetrievalQAChain } from "langchain/chains";
 import { OpenAI } from "langchain/llms/openai";
+
 import * as dotenv from "dotenv";
 
 dotenv.config();
 
-const loader = new PDFLoader("src/documents/budget_speech.pdf");
+const loader = new CSVLoader("src/documents/constituents-financials_csv.csv");
 
 const docs = await loader.load();
 
-// splitter function
 const splitter = new RecursiveCharacterTextSplitter({
-  chunkSize: 1000,
-  chunkOverlap: 20,
+    chunkSize: 1000,
+    chunkOverlap: 200,
 });
 
-// created chunks from pdf
+// chunks are created
 const splittedDocs = await splitter.splitDocuments(docs);
 
-const embeddings = new OpenAIEmbeddings();
+const embeddings = new OpenAIEmbeddings({
+    openAIApiKey: process.env.OPENAI_API_KEY
+});
 
 const vectorStore = await HNSWLib.fromDocuments(
-  splittedDocs,
-  embeddings
+    splittedDocs,
+    embeddings
 );
 
-const vectorStoreRetriever = vectorStore.asRetriever();
+// init LLM MOdel 
 const model = new OpenAI({
-  modelName: 'gpt-3.5-turbo'
+    modelName: 'gpt-3.5-turbo'
 });
+
+const vectorStoreRetriever = vectorStore.asRetriever();
 
 const chain = RetrievalQAChain.fromLLM(model, vectorStoreRetriever);
 
-const question = 'What is the theme of G20?';
+const question = "What is the EBITDA for Advance Auto Parts?";
 
 const answer = await chain.call({
-  query: question
+    query: question
 });
 
 console.log({
-  question,
-  answer
+    question, 
+    answer
 });
 
-const question1 = 'What is the budget allocated for railways?';
+const question1 = "What is the dividend yield for Advance Auto Parts?";
+
 const answer1 = await chain.call({
-  query: question1
+    query: question1
 });
 
 console.log({
-  question: question1,
-  answer: answer1
+    question: question1, 
+    answer: answer1
 });
 
-const question2 = 'what is PM Matsya Sampada Yojana?'
-const answer2 = await chain.call({
-  query: question2
-});
-console.log({
-  question: question2,
-  answer: answer2
-});
 
 
 
